85622c542d0dfc424661dcfe0a442d0b6ad45490
yassine-alaoui <alaoui6b@gmail.com>
2021-04-03 16:25:22 +0100

    lol

--- module00/ex01/exec.py
+++ module00/ex01/exec.py
@@ -1,12 +1,6 @@
 import sys
 if len(sys.argv) > 1:
 	txt = ""
-	prev = ""
-	for i, arg in enumerate(sys.argv):
-		if i > 0:
-			prev = txt
-			txt = arg[::-1].swapcase()
-			if i - 1:
-				txt += " "
-			txt += prev
-	print(txt)
+	for arg in sys.argv[1:]:
+			txt = arg[::-1].swapcase() + " " + txt
+	print(txt[:-1])
