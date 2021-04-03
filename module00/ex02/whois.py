fbcb7c5201a19192fc69ce39ef1cb03b1e0b6acb
yassine-alaoui <alaoui6b@gmail.com>
2021-04-03 16:29:02 +0100

    error

--- module00/ex02/whois.py
+++ module00/ex02/whois.py
@@ -1,11 +1,11 @@
 import sys
-map = ["I'm Even.", "I'm Odd.", "I'm Zero."]
-if (len(sys.argv) == 2):
+arr = ["I'm Even.", "I'm Odd.", "I'm Zero."]
+if len(sys.argv) == 2 and sys.argv[1].isdigit():
 	num = int(sys.argv[1])
 	if (num == 0):
-		print(map[2])
+		print(arr[2])
 	else:
-		print(map[num%2])
+		print(arr[num%2])
 else:
 	print("ERROR")
 
