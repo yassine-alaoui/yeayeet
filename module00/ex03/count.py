7864f98485948f429b2ecf40dbd5dd7c7988119a
yassine-alaoui <alaoui6b@gmail.com>
2021-04-03 16:45:34 +0100

    modif

--- module00/ex03/count.py
+++ module00/ex03/count.py
@@ -1,26 +1,13 @@
 def counter(string):
-	upper = 0
-	lower = 0
-	space = 0
-	puncs = 0
-	for char in string:
-		if char == ' ':
-			space += 1
-		elif char.isupper():
-			upper += 1
-		elif char.islower():
-			lower += 1
-		else:
-			puncs += 1
-	print("- " + str(upper)+ " upper letters")
-	print("- " + str(lower)+ " lower letters")
-	print("- " + str(puncs)+ " punctuation marks")
-	print("- " + str(space)+ " spaces")
+	print("- " + str(sum([char.isupper() for char in string])) + " upper letters")
+	print("- " + str(sum([char.islower() for char in string])) + " lower letters")
+	print("- " + str(sum([char in ",;:." for char in string])) + " punctuation marks")
+	print("- " + str(string.count(" ")) + " spaces")
 
 def text_analyzer(*string):
 	if len(string) == 0:
 		counter(str(input("What is the text to analyse?\n>>>")))
-	elif len(string) > 0:
+	elif len(string) > 1:
 		print("ERROR")
 	else:
 		counter(string[0])
